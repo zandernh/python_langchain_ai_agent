0147ace81c6527590b67cc60d119f2304a5904c2
unknown <zandernh@gmail.com>
2025-04-03 07:52:45 +0200

    Uploading project files| created 3 tools for model to use

--- main.py
+++ main.py
@@ -2,13 +2,58 @@ import os
 from dotenv import load_dotenv
 from pydantic import BaseModel
 from langchain_openai import ChatOpenAI
-from langchain_anthropic import ChatAnthropic
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.output_parsers import PydanticOutputParser
+from langchain.agents import create_tool_calling_agent, AgentExecutor
+from tools import search_tool, wiki_tool, save_tool
 
 load_dotenv()
 
 api_key = os.getenv("OPENAI_API_KEY")
 
-llm = ChatOpenAI(model="gpt-4o", api_key=api_key)
+class ResearchResponse(BaseModel):
+    topic: str
+    summary: str
+    sources: list[str]
+    tools_used: list[str]
 
-response = llm.invoke("Give me a quote from George Washington")
-print(response)
+llm = ChatOpenAI(model="gpt-4-turbo", api_key=api_key)
+
+parser = PydanticOutputParser(pydantic_object=ResearchResponse) 
+
+prompt = ChatPromptTemplate.from_messages(
+    [
+        (
+            "system",
+            """
+            You are a research assistant that will help generate a research paper. 
+            Answer the user query and use necessary tools.
+            Wrap the output in this format and provide no other text\n{format_instructions}
+            """,
+        ),
+        ("placeholder", "{chat_history}"),
+        ("human", "{query}"),
+        ("placeholder", "{agent_scratchpad}"),
+
+    ]
+).partial(format_instructions=parser.get_format_instructions())
+
+tools = [search_tool, wiki_tool, save_tool]
+
+agent = create_tool_calling_agent(
+    llm=llm,
+    prompt=prompt,
+    tools = tools,
+)
+
+agent_executor = AgentExecutor(agent=agent, tools=tools, verbose=True)
+
+query = input("What can I help you research?")
+
+raw_response = agent_executor.invoke({"query": query})
+
+try:
+    structured_response = parser.parse(raw_response.get("output")[0]["text"])
+    print(structured_response)
+except Exception as e:
+    print("Error parsing response", e, "Raw Response:\t", raw_response)
